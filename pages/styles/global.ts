b7870ceae1c74b83670a5b6547ed701f2a053456
Matt Gunnin <mgunnin@esports.one>
2023-12-21 16:54:18 -0600

    Update dependencies and global styles

--- pages/styles/global.ts
+++ pages/styles/global.ts
@@ -1,14 +1,27 @@
 import { createGlobalStyle } from "styled-components";
 
-export default createGlobalStyle`@import url('https://fonts.googleapis.com/css2?family=Loos+Wide:wght@500;700&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Aquire:wght@700&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&display=swap');
+export default createGlobalStyle`
 @font-face {
         font-family: "Aquire";
-        src: url("/public/Aquire Bold.otf");
+        src: url("/public/fonts/Aquire-Bold.otf");
         font-weight: 700;
       }
-
+      @font-face {
+        font-family: "Montserrat";
+        src: url("/public/fonts/Montserrat-Bold.ttf");
+        font-weight: 700;
+      }
+      @font-face {
+        font-family: "LoosWide";
+        src: url("/public/fonts/LoosWide-Bold.otf");
+        font-weight: 700;
+      }
+    *, *::before, *::after {
+      box-sizing: border-box;
+    }
+    html, body, #__next {
+      height: 100%;
+    }
     body {
       margin: 0; line-height: normal;
     }
