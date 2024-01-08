2d742214d6d8b0abf64a1716f7e3112f4e9e9123
mgunnin <mgunnin>
2024-01-08 23:56:37 +0000

    Pushed from Locofy Builder

--- pages/styles/global.tsx
+++ pages/styles/global.tsx
@@ -1,9 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
-export default createGlobalStyle`@import url('https://fonts.googleapis.com/css2?family=Aquire:wght@700&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Suisse+Intl:wght@600;700&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Loos+Wide:wght@500;700&display=swap');
+export default createGlobalStyle`
 @font-face {
         font-family: "Aquire";
         src: url("/public/fonts/Aquire-Bold.otf");
